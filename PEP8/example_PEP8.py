578ae89efa91a83708b75081c2fe29ff06685703
Matheus Feu <101346300+Havokzbr@users.noreply.github.com>
2023-03-27 21:21:08 -0300

    Python Enhancement Proposal

---
+++ PEP8/example_PEP8.py
@@ -0,0 +1,60 @@
+"""
+PEP8 - Python Enhancement Proposal (São propostas de melhorias na linguagem Python)
+
+The Zen of Python - import this
+
+O propósito da PEP8 é fazer que nosso código Python seja Pythonico.
+
+[1] - Utilize Camel Case para nomes de classes;
+[2] - Utilize nomes em minúsculo, separados por underline para funções ou variáveis;
+[3] - Utilize 4 espaços para identação! (Não use tab);
+[4] - Linhas em branco;
+- Separar funções e definições de classe com duas linhas em branco;
+- Métodos dentro de uma classe devem ser separados com uma única linha em branco;
+[5] - Imports devem ser sempre feitos em linhas separadas;
+- Devem ser colocados no topo do arquivo, logo após quaisquer comentários ou docstrings e antes de constantes ou variáveis globais;
+[6] - Espaços em expressões e instruções;
+[7] - Termine sempre uma instrução com uma nova linha;
+"""
+
+# [1]
+class Calculadora:
+    """Classe de exemplo de como utilizar Camel Case"""
+    pass
+
+
+# [2]
+def soma_dois_numeros():
+    """Função de exemplo de como utilizar minúsculo e underline"""
+    pass
+
+
+numero = 4
+numero_impar = 9
+
+# [3]
+if 'a' in 'banana':
+    print('tem')
+
+# [4]
+variavel = 10
+print(variavel)
+
+# [5]
+import sys
+import os
+
+# [6]
+funcao(algo[1], {outro: 2})
+
+dict['chave'] = list[indice]
+
+x = 1
+y = 3
+variavel_longa = 5
+
+
+
+
+
+
